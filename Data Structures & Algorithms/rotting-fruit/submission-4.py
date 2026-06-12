6bfd4d9191d9ad7a75fd76178bb2c99a66d797f5
echou0321 <166260783+echou0321@users.noreply.github.com>
2026-06-12 15:00:52 -0700

    Add: rotting-fruit - submission-4

---
+++ Data Structures & Algorithms/rotting-fruit/submission-4.py
@@ -0,0 +1,35 @@
+class Solution:
+    def orangesRotting(self, grid: List[List[int]]) -> int:
+        rows, cols = len(grid), len(grid[0])
+        time = 0
+        fresh = 0
+        q = deque()
+        
+        def traverse(r, c):
+            nonlocal fresh
+            if 0 <= r < rows and 0 <= c < cols and grid[r][c] == 1:
+                grid[r][c] = 2
+                q.append((r, c))
+                fresh -= 1
+
+        for r in range(rows):
+            for c in range(cols):
+                if grid[r][c] == 1:
+                    fresh += 1
+                if grid[r][c] == 2:
+                    q.append((r, c))
+
+        while q and fresh > 0:
+            for i in range(len(q)):    
+                r, c = q.popleft()
+
+                traverse(r + 1, c)
+                traverse(r - 1, c)
+                traverse(r, c + 1)
+                traverse(r, c - 1)
+            time += 1
+        
+        return time if fresh == 0 else -1
+
+
+        
