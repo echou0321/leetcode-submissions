0a4e5a1ddbd85a1f4e1a58893aede4a48fbd57cb
echou0321 <166260783+echou0321@users.noreply.github.com>
2026-06-22 13:28:19 -0700

    Add: search-for-word - submission-1

---
+++ Data Structures & Algorithms/search-for-word/submission-1.py
@@ -0,0 +1,26 @@
+class Solution:
+    def exist(self, grid: List[List[str]], word: str) -> bool:
+        rows, cols = len(grid), len(grid[0])
+
+        def backtrack(r, c, i):
+            if i == len(word):
+                return True
+            if r < 0 or r >= rows or c < 0 or c >= cols or grid[r][c] == "#" or grid[r][c] != word[i]:
+                return False
+            grid[r][c] = "#"
+
+            res = (backtrack(r + 1, c, i + 1) or
+                  backtrack(r - 1, c, i + 1) or
+                  backtrack(r, c + 1, i + 1) or
+                  backtrack(r, c - 1, i + 1)) 
+            grid[r][c] = word[i]
+            return res
+        
+        for r in range(rows):
+            for c in range(cols):
+                if grid[r][c] == word[0]:
+                    if backtrack(r, c, 0):
+                        return True
+        
+        return False
+            
