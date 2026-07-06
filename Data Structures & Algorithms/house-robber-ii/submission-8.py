e10260a0ef581dcece5d287edbe23c7b17492aba
echou0321 <166260783+echou0321@users.noreply.github.com>
2026-07-05 21:45:24 -0700

    Add: house-robber-ii - submission-8

---
+++ Data Structures & Algorithms/house-robber-ii/submission-8.py
@@ -0,0 +1,14 @@
+class Solution:
+    def rob(self, nums: List[int]) -> int:
+        if len(nums) == 1: return nums[0]
+        return max(self.robber(nums[1:]), self.robber(nums[:-1]))
+    
+    def robber(self, nums):
+        prev1, prev2 = 0, 0
+
+        for num in nums:
+            current = max(prev1, prev2 + num)
+            prev2 = prev1
+            prev1 = current
+        
+        return prev1
