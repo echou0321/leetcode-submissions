34174e5682982242ba24fe8a6562b5a85efbcd14
echou0321 <166260783+echou0321@users.noreply.github.com>
2026-07-25 17:39:37 -0700

    Add: sql-sellers-with-no-sales - submission-0

---
+++ SQL/sql-sellers-with-no-sales/submission-0.sql
@@ -0,0 +1,6 @@
+-- Write your query below
+SELECT s.seller_name 
+FROM seller s
+LEFT JOIN orders o on o.seller_id = s.seller_id AND EXTRACT(YEAR FROM o.sale_date) = 2020
+WHERE o.order_id IS NULL
+ORDER BY seller_name ASC
